60818db788b5cc62757c50446686cd6f5fa68d00
taberhuang <taber.huang@gmail.com>
2012-11-30 10:14:20 +0800

    add cocos2d-html5 support

---
+++ public/javascripts/brag/resource.js
@@ -0,0 +1,20 @@
+var s_HelloWorld = "resources/HelloWorld.png";
+var s_CloseNormal = "resources/CloseNormal.png";
+var s_CloseSelected = "resources/CloseSelected.png";
+
+var g_ressources = [
+    //image
+    {type:"image", src:s_HelloWorld},
+    {type:"image", src:s_CloseNormal},
+    {type:"image", src:s_CloseSelected}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
