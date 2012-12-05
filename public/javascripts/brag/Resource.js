ec95acf1f3e6898f2084f339bca496ea41eadc84
taberhuang <taber.huang@gmail.com>
2012-12-05 08:09:45 +0800

    update

---
+++ public/javascripts/brag/Resource.js
@@ -0,0 +1,11 @@
+
+var dirImg = 'resources/image/';
+var dirMusic = 'resources/music/';
+
+//image 
+var s_bg = dirImg + 'bg.jpg';
+var s_avatar_bg = dirImg + 'avatar-bg.png';
+
+var g_ressources = [
+    {type:'image', src:s_bg}
+];
