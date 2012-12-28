207b6c5ecf50bd0c1e59221370e03488e2a8d8e5
taberhuang <taber.huang@gmail.com>
2012-12-28 16:04:00 +0800

    修改卡片按纽的自定义

--- public/javascripts/brag/CardMenuItem.js
+++ public/javascripts/brag/CardMenuItem.js
@@ -18,17 +18,22 @@
 var CardMenuItem = cc.MenuItemSprite.extend({
 
     init: function(card) {
-        this._super(s_card_bg);
+        var normalSprite = cc.Sprite.create(s_card_bg);
+
+        this.initWithNormalSprite(normalSprite);
 
         var s_r = cc.rect((card.suit-1) * 20, 0, 20, 19);
-        var v_r = cc.rect((card.value-1) * 50, 0, 50, 40);
-        var suit = cc.Sprite.create(s_card_suit, s_r);
-        var value = cc.Sprite.create(s_card_value, v_r);
+        var v_r = cc.rect((card.value-1) * 40, 0, 40, 40);
+        var suitSprite = cc.Sprite.create(s_card_suit, s_r);
+        var valueSprite = cc.Sprite.create(s_card_value, v_r);
 
-        suit.setPosition(cc.p(40, 30));
+        suitSprite.setPosition(cc.p(30, 10));
+        valueSprite.setPosition(cc.p(20, 30));
 
-        this.addChild(suit);
-        this.addChild(value);
+        this.addChild(suitSprite);
+        this.addChild(valueSprite);
+
+        this.index = card.index;
     }
 });
 
